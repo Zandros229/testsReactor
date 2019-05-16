62c8feb56c4c974dfe0f1a20b12ecc59ed6602a4
Zandros229 <ernest.figura548@gmail.com>
2019-05-16 17:55:56 +0200

    Refactor code and fix tests after refactoring then add some tests

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -35,7 +35,9 @@ public class AtmMachineTest {
         cardProviderService = mock(CardProviderService.class);
         bankService = mock(BankService.class);
         moneyDepot = mock(MoneyDepot.class);
-
+        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
+        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
+        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
 
         atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
 
@@ -43,9 +45,6 @@ public class AtmMachineTest {
 
     @Test
     public void ShouldWItchDraw200WhenWithDraw200WithEnoughMoneyOnBankAccountTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
         when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
@@ -55,12 +54,35 @@ public class AtmMachineTest {
 
         Assertions.assertEquals(payment.getValue(),atmMachine.withdraw(money,card).getValue());
     }
+    @Test
+    public void ShouldWItchDraw500WhenWithDraw500WithEnoughMoneyOnBankAccountTest(){
+        money=Money.builder().withAmount(500).withCurrency(Currency.PL).build();
+        when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
+        when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
+        when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
+
+        List<Banknote> expeted=Banknote.forCurrency(Currency.PL);
+        payment=new Payment(expeted.subList(5,6));
+
+        Assertions.assertEquals(payment.getValue(),atmMachine.withdraw(money,card).getValue());
+    }
+
+    @Test
+    public void ShouldWItchDraw200WhenWithDraw200WithCurrencyEUWithEnoughMoneyOnBankAccountTest(){
+        money=Money.builder().withAmount(200).withCurrency(Currency.EU).build();
+        when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
+        when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
+        when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
+
+        List<Banknote> expeted=Banknote.forCurrency(Currency.EU);
+        payment=new Payment(expeted.subList(4,5));
+
+        Assertions.assertEquals(payment.getValue(),atmMachine.withdraw(money,card).getValue());
+    }
 
     @Test
     public void ShouldInvokeMoneyDeptExcetionWhenInvokeWithdrawOnATMMachineWithMoneyDeptProblemsTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
+
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
 
@@ -69,27 +91,19 @@ public class AtmMachineTest {
 
     @Test
     public void ShouldInvokeInsufficientFundsExceptionWhenOnChargeAccDOntHaveEnoghMoney(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
 
         Assertions.assertThrows(InsufficientFundsException.class,()->atmMachine.withdraw(money,card).getValue());
     }
     @Test
     public void ShouldInvokeCardAutorizationExceptionWhenCardProviderServiceDOntFindTHisCardOrItsBrokenTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
 
         Assertions.assertThrows(CardAuthorizationException.class,()->atmMachine.withdraw(money,card).getValue());
     }
 
     @Test
     public void ShouldInvokeWrongMoneyAmountExceptionWhenAmountOfMoneyToWithDrawIsLowerThanZeroTest(){
-        money=Money.builder().withAmount(-50).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
+        money=Money.builder().withAmount(-15).withCurrency(Currency.PL).build();
 
         Assertions.assertThrows(WrongMoneyAmountException.class,()->atmMachine.withdraw(money,card).getValue());
     }
@@ -97,17 +111,11 @@ public class AtmMachineTest {
     @Test
     public void ShouldInvokeWrongMoneyAmountExceptionWhenAmountOfMoneyToWithDrawIsThanZeroTest(){
         money=Money.builder().withAmount(0).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
-
         Assertions.assertThrows(WrongMoneyAmountException.class,()->atmMachine.withdraw(money,card).getValue());
     }
 
     @Test
     public void ShouldInvokeOnceMethodAuthorizeOnCardProviderServiceWhenMakingWithDrawTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
         when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
@@ -119,9 +127,6 @@ public class AtmMachineTest {
 
     @Test
     public void ShouldInvokeOnceMethodStartTransactionOnBankServiceWhenMakingWithDrawTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
         when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
@@ -133,9 +138,6 @@ public class AtmMachineTest {
 
     @Test
     public void ShouldInvokeOnceMethodCommitOnBankServiceWhenMakingWithDrawTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
         when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
@@ -147,9 +149,6 @@ public class AtmMachineTest {
 
     @Test
     public void ShouldInvokeOnceMethodChargeOnBankServiceWhenMakingWithDrawTest(){
-        money=Money.builder().withAmount(200).withCurrency(Currency.PL).build();
-        card=Card.builder().withCardNumber("12345").withPinNumber(1234).build();
-        authenticationToken=AuthenticationToken.builder().withAuthorizationCode(1234).withUserId("12345").build();
         when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
         when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
         when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
@@ -157,6 +156,17 @@ public class AtmMachineTest {
 
 
         verify(bankService,times(1)).charge(authenticationToken,money);
+    }
+
+    @Test
+    public void ShouldInvokeOnceMethodReleaseBanknotesOnMoneyDepotWhenMakingWithDrawTest(){
+        when(cardProviderService.authorize(card)).thenReturn(java.util.Optional.ofNullable(authenticationToken));
+        when(bankService.charge(authenticationToken,money)).thenReturn(Boolean.TRUE);
+        when(moneyDepot.releaseBanknotes((List<Banknote>)notNull())).thenReturn(Boolean.TRUE);
+        atmMachine.withdraw(money,card);
+
+
+        verify(moneyDepot,times(1)).releaseBanknotes((List<Banknote>)notNull());
     }
 
 }
