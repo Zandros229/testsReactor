d50f5eb9d2e66a7d228fbed4d2de0d7c21ba8fe0
Zandros229 <ernest.figura548@gmail.com>
2019-05-16 16:41:58 +0200

    Add dependecy and mock parameters

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,15 +1,26 @@
 package edu.iis.mto.testreactor.exc3;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import org.junit.Test;
 
+@ExtendWith(MockitoExtension.class)
 public class AtmMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
-    }
+    @Mock
+    private CardProviderService cardProviderService;
+    @Mock
+    private BankService bankService;
+    @Mock
+    private MoneyDepot moneyDepot;
+
+    private AtmMachine atmMachine;
+
+
+
 
 }
