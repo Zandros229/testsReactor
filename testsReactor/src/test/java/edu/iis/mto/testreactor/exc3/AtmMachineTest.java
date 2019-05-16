fca6e1d70c3796bb99cf014eae7a38a75ffc4e18
Zandros229 <ernest.figura548@gmail.com>
2019-05-16 16:47:29 +0200

    Add some Beforeeach setup

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -7,19 +7,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.mock;
+
 
 @ExtendWith(MockitoExtension.class)
-public class AtmMachineTest {
+    public class AtmMachineTest {
 
-    @Mock
-    private CardProviderService cardProviderService;
-    @Mock
-    private BankService bankService;
-    @Mock
-    private MoneyDepot moneyDepot;
+        @Mock
+        private CardProviderService cardProviderService;
+        @Mock
+        private BankService bankService;
+        @Mock
+        private MoneyDepot moneyDepot;
 
-    private AtmMachine atmMachine;
+        private AtmMachine atmMachine;
 
+        @BeforeEach
+        public void setup(){
+            cardProviderService=mock(CardProviderService.class);
+            bankService=mock(BankService.class);
+            moneyDepot=mock(MoneyDepot.class);
+        }
 
 
 
